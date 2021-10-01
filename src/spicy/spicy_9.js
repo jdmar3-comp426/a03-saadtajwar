bc78f6aa10307625ac8b308a6420ec4c50d6d761
Saad Tajwar <saads@ad.unc.edu>
2021-09-30 20:00:19 -0400

    Spicy functions

--- src/spicy/spicy_9.js
+++ src/spicy/spicy_9.js
@@ -16,6 +16,12 @@
  */
 export const repeat = (fn, n, ...params) => {
 
+    const result = [];
+    for (let i = 0; i < n; i++) {
+        result.concat(fn(params));
+    }
+
+    return result;
 };
 
 
@@ -59,7 +65,10 @@ export const multiplyBy = (num1) => {
  * Use the multiplyBy function to create and export a function named
  *   "tenTimes" that multiplies a number by 10.
  */
-export const tenTimes = undefined;
+export const tenTimes = (num1) => {
+    const multiplied = multiplyBy(10);
+    return multiplied(num1);
+}
 
 
 /**
@@ -68,7 +77,10 @@ export const tenTimes = undefined;
  */
 export const tenTimesFifty = () => {
 
-};
+    return tenTimes(50);
+
+}
+
 
 
 /**************************************************************************
@@ -96,6 +108,14 @@ export const tenTimesFifty = () => {
  */
 export const everyEven = (arr, test) => {
 
+    for (let i = 0; i < arr.length; i += 2) {
+        if (!test(arr[i])) {
+            return false;
+        }
+    }
+
+    return true;
+
 };
 
 
@@ -119,6 +139,14 @@ export const everyEven = (arr, test) => {
  *    someEven([0, 0, 0, 0, 0], x => x === 0)  <--  returns true
  */
 export const someEven = (arr, test) => {
+
+    for (let i = 0; i < arr.length; i += 2) {
+        if (test(arr[i])) {
+            return true;
+        }
+    }
+
+    return false;
 
 };
 
@@ -156,6 +184,16 @@ export const filter = (arr, test) => {
  */
 export const allEvensAreOdd = (arr) => {
 
+    const checkOdd = (num) => {
+        if (num % 2 !== 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    return everyEven(arr, checkOdd);
+
 };
 
 
@@ -165,6 +203,16 @@ export const allEvensAreOdd = (arr) => {
  *   array is an odd number. Use the "someEven" function in this function.
  */
 export const anEvenIsOdd = (arr) => {
+
+    const checkOdd = (num) => {
+        if (num % 2 !== 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    return someEven(arr, checkOdd);
 
 };
 
@@ -176,5 +224,7 @@ export const anEvenIsOdd = (arr) => {
  *   pass the test. You must use the filter function.
  */
 export const hasExactly = (arr, test, n) => {
+
+    
 
 };
