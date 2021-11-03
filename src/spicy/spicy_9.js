15c7cf770e80efc2b71c799ba053da5b8a3fd34a
Saad Tajwar <saads@ad.unc.edu>
2021-11-03 15:06:48 -0400

    Spicy9

--- src/spicy/spicy_9.js
+++ src/spicy/spicy_9.js
@@ -18,7 +18,7 @@ export const repeat = (fn, n, ...params) => {
 
     let result = [];
     for (let i = 0; i < n; i++) {
-        result.push(fn(params));
+        result.push(fn(...params));
     }
 
     return result;
